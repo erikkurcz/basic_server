d5b82ea8d807501efa7e2fef2227c100ba01f3bb
Erik Kurcz <ejkucz@gmail.com>
2021-07-27 00:55:31 -0400

    add in sleep time option, ignore failure on removing sockpath

--- server.cpp
+++ server.cpp
@@ -14,12 +14,27 @@
 #include "sock_location.hh"
 
 
+void usage(void){
+    printf("mysever N\n\nWhere N is number of seconds to sleep between writing chars to client\n");
+}
+
 int main(int argc, char* argv[]){
 
-    // Basics
+    // Get sleep time between chars off cmdline
+    if (argc > 2){
+        usage();
+        exit(1);
+    }
+
+    int sleep_seconds(0);
+    if (argc == 2){
+        sleep_seconds = atoi(argv[1]);
+    }
+
+    // Basic
     int BUFFER_SIZE = 1024;
     int CONNECTION_BACKLOG = 10;
-    
+
     // Buffer and related reading and writing
     char buf[BUFFER_SIZE];
     int read_ct(0);
@@ -33,20 +48,23 @@ int main(int argc, char* argv[]){
     socklen_t their_address_size;
 
     // Remove the sock path before we begin just in case
-    if (system("rm -f SOCKPATH") == -1){
-        std::cerr << "Failed to remove SOCK_PATH: `" << SOCK_PATH << "`, errno: " << errno << ": " << strerror(errno) << std::endl;
-        return -1;
-    }
+    // Don't really care if it fails
+    remove(SOCK_PATH);
 
-    // Open up a connection 
+//    if (remove(SOCK_PATH) == -1){
+//        std::cerr << "Failed to remove SOCK_PATH: `" << SOCK_PATH << "`, errno: " << errno << ": " << strerror(errno) << std::endl;
+//        return -1;
+//    }
+
+    // Open up a connection
     mysock = socket(AF_UNIX, SOCK_STREAM, 0);
     if (mysock == -1){
         std::cerr << "Socket failed to open, errno: " << errno << ": " << strerror(errno) << std::endl;
         return -1;
     }
-    
+
     // Bind to it
-   
+
     // Make necessary structs
     // Clear first then fill
     memset(&my_address, 0, sizeof(struct sockaddr));
@@ -64,7 +82,7 @@ int main(int argc, char* argv[]){
         std::cerr << "Failed to listen, errno: " << errno << ": " << strerror(errno) << std::endl;
         return -1;
     }
-    
+
     // Wait for connection
     std::cout << "Waiting for connection before accepting data to send..." << std::endl;
     their_address_size = sizeof(struct sockaddr);
@@ -73,18 +91,25 @@ int main(int argc, char* argv[]){
         std::cerr << "Failed to accept their sock, errno: " << errno << ": " << strerror(errno) << std::endl;
         return -1;
     } else {
-        std::cout << "Master has given Dobby a sock!\n" 
+        std::cout << "Master has given Dobby a sock!\n"
                   << "Connected! Begin writing." << std::endl;
     }
 
     // Read off command line until Ctrl+C
     errno = 0;
+    int to_write(0);
     while (read_ct = read(STDIN_FILENO, &buf, BUFFER_SIZE)){
-        
+
         // Send ittttt
-        while (read_ct > 0){ 
+        while (read_ct > 0){
+
+            // sly fox sleeps between each read
+            if (sleep_seconds > 0){
+                sleep(sleep_seconds);
+            }
 
             written_ct = write(theirsock, &buf, read_ct);
+
             if (written_ct == -1){
                 std::cerr << "Failed to write to their sock, errno: " << errno << ": " << strerror(errno) << std::endl;
                 return -1;
@@ -101,7 +126,7 @@ int main(int argc, char* argv[]){
         std::cerr << "Failed to read from command line, errno: " << errno << ": " << strerror(errno) << std::endl;
         return -1;
     }
-    
+
     // Remove the sock path
     if (remove(SOCK_PATH) == -1){
         std::cerr << "Failed to remove SOCK_PATH: `" << SOCK_PATH << "`, errno: " << errno << ": " << strerror(errno) << std::endl;
