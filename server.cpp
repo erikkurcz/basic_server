3aed28c2b358addd5bfb591727905b13713102a6
Erik Kurcz <ejkucz@gmail.com>
2022-01-17 17:13:50 -0500

    add means to ignore sigpipe, switch to send() to ignore signals as well (which supersedes), handle errno and progress beyond. now only issue is if first client drops, no others receive data

--- server.cpp
+++ server.cpp
@@ -4,6 +4,7 @@
 #include <string>
 
 #include <netinet/ip.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,12 +20,25 @@ void usage(void){
     printf("mysever N\n\nWhere N is number of seconds to sleep between writing chars to client\n");
 }
 
+void handle_sigpipe(int signo){
+    std::cout << "SIGPIPE received, likely client connection dropped.\n\nOh no! Anyways..." << std::endl;
+}
+
 int main(int argc, char* argv[]){
 
     // Get sleep time between chars off cmdline
     if (argc > 2){
         usage();
         exit(1);
+    }
+
+    // set signal handler for SIGPIPE only
+    struct sigaction sa;;
+    sa.sa_handler = &handle_sigpipe;
+    sa.sa_handler = SIG_IGN;
+    if (sigaction(SIGPIPE, &sa, NULL) != 0){ 
+        std::cerr << "Failed to set signal handler, errno: " << errno << strerror(errno) << std::endl;
+        return -1;
     }
 
     int sleep_seconds(0);
@@ -136,27 +150,50 @@ int main(int argc, char* argv[]){
                             sleep(sleep_seconds);
                         }
                         std::cout << "verifying connection is alive @ idx: " << idx << " to fd=" << connection_arr[idx] << std::endl;
-                        read_rc = read(connection_arr[idx],&readbuf,0);
+                        read_rc = read(connection_arr[idx], &readbuf, 0);
                         if (read_rc == -1){
                             std::cout << "connection @ idx " << idx << " to fd=" << connection_arr[idx] << " is invalid: errno: " << errno << std::endl;
                             return -1;
                         }
-                        std::cout << "writing to connection @ idx: " << idx << " to fd=" << connection_arr[idx] << std::endl;
-                        errno = 0;
-                        written_ct = write(connection_arr[idx], &buf, local_read_ct);
-                        // write might not return -1 in event of a socket connection lost/closed by the client side, but errno would be set to EAGAIN or EWOULDBLOCK in event it's a non-blocking socket but the write would block
-                        // check for that here and alter the idx position if this is the case
-                        if (errno == EAGAIN || errno == EWOULDBLOCK){
-                            std::cout << "writing to connection @ idx: " << idx << " to fd=" << connection_arr[idx] << " failed to write properly, removing from list" << std::endl;
-                            connection_arr[idx] = 0;
-                        }
 
-                        if (written_ct == -1){
-                            std::cerr << "Failed to write to their sock, errno: " << errno << ": " << strerror(errno) << ", idx=" << idx << ", fd=" << connection_arr[idx] << std::endl;
-                            return -1;
+                        if (readbuf[0] == 'x'){ 
+                            // client disconnected, set to zero
+                            std::cout << "client at connection @ idx: " << idx << " has disconnected" << std::endl;
+                            connection_arr[idx] = 0;
+                            sock_count--;
+
                         } else {
-                            // So we keep going if we write fewer than the read message
-                            local_read_ct = local_read_ct - written_ct;
+
+                            // client still there, write to it
+
+                            std::cout << "writing to connection @ idx: " << idx << " to fd=" << connection_arr[idx] << std::endl;
+                            errno = 0;
+                            written_ct = send(connection_arr[idx], &buf, local_read_ct, MSG_NOSIGNAL);
+                            // write might not return -1 in event of a socket connection lost/closed by the client side, but errno would be set to EAGAIN or EWOULDBLOCK in event it's a non-blocking socket but the write would block
+                            // check for that here and alter the idx position if this is the case
+                            if (errno == EAGAIN || errno == EWOULDBLOCK){
+                                std::cout << "writing to connection @ idx: " << idx << " to fd=" << connection_arr[idx] << " failed to write properly, removing from list" << std::endl;
+                                connection_arr[idx] = 0;
+                                sock_count--;
+                                break;
+                            }
+
+                            if (errno == EPIPE){
+                                std::cout << "send() to connection @ idx: " << idx << " to fd=" << connection_arr[idx] << " failed to send, received EPIPE, likely dropped connection and am removing from list" << std::endl;
+                                connection_arr[idx] = 0;
+                                sock_count--;
+                                written_ct = 0;
+                                idx--;
+                                break;
+                            }
+
+                            if (written_ct == -1){
+                                std::cerr << "Failed to write to their sock, errno: " << errno << ": " << strerror(errno) << ", idx=" << idx << ", fd=" << connection_arr[idx] << std::endl;
+                                return -1;
+                            } else {
+                                // So we keep going if we write fewer than the read message
+                                local_read_ct = local_read_ct - written_ct;
+                            }
                         }
                     }
                 }
