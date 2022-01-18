4b7b1ecc7342df0c1904f285dd3ff830ff7c9e41
Erik Kurcz <ejkucz@gmail.com>
2022-01-17 20:04:48 -0500

    add mechanism for client to know it wont be accepted and error out

--- client.cpp
+++ client.cpp
@@ -14,8 +14,18 @@
 
 #include "sock_location.hh"
 
+void usage(){
+    printf("Usage: ./myclient PORT\n\nWhere\n\tPORT is the port number to connect on\n");
+}
 
 int main(int argc, char* argv[]){
+
+    if (argc != 2){
+        usage();
+        return 1;
+    }
+
+    const int PORT_NUMBER = atoi(argv[1]);
 
     // Basics
     int BUFFER_SIZE = 1024;
@@ -52,15 +62,19 @@ int main(int argc, char* argv[]){
     }
 
     // Read off the connection foreverrrr
-    while (read_ct = read(mysock, &buf, BUFFER_SIZE)){
+    while (read_ct = recv(mysock, &buf, BUFFER_SIZE, 0)){
 
         if (read_ct == -1){
             std::cerr << "Failed to read from socket, errno: " << errno << ": " << strerror(errno) << std::endl;
             return -1;
         }
-        
+
         // Write it to cmdline
         while (read_ct > 0){ 
+            if (strncmp(&buf[0], &MAX_CONNECTIONS_RETRY_FLAG, 1) == 0){
+                std::cerr << "Server at max connections, retry later" << std::endl;
+                return 1;
+            }
             written_ct = write(STDOUT_FILENO, &buf, read_ct);
             if (written_ct == -1){
                 std::cerr << "Failed to write to their sock, errno: " << errno << ": " << strerror(errno) << std::endl;
--- sock_location.hh
+++ sock_location.hh
@@ -3,5 +3,6 @@
 #define SOCK_LOCATION_H
 
 const char* SOCK_PATH = "/tmp/TMP_SOCK";
-const int PORT_NUMBER = 12728;
+const char MAX_CONNECTIONS_RETRY_FLAG = '`';
+const int MAX_CONNECTIONS_RETRY_FLAG_SIZE = 1; 
 #endif 
