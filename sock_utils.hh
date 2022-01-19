55872e121f4e1ec587689e83549f6fc320564406
Erik Kurcz <ejkucz@gmail.com>
2022-01-19 01:12:54 -0500

    add functon for checking if a fd has data to be read

--- sock_utils.hh
+++ sock_utils.hh
@@ -10,4 +10,29 @@ const char MAXCONNBUF[MAXCONNBUF_SIZE] = { 'M','A','X','C','O','N','N' };
 
 const int PINGBUF_SIZE = 4;
 const char PINGBUF[PINGBUF_SIZE] = { 'P','I','N','G' };
+
+int has_data(int fd, bool debug=false){
+    // Performs a select to determine if this fd can be used for a read immediately
+    // 0 if no, 1 if yes, -1 if error
+    int num_fds(fd+1);
+
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0; 
+
+    int timeout = 0;
+
+    fd_set read_watch_fds;
+    FD_ZERO(&read_watch_fds);
+    FD_SET(fd, &read_watch_fds);
+
+    int rc = select(num_fds, &read_watch_fds, NULL, NULL, &tv);
+    if (rc == -1){
+        std::cerr << "Error while checking select() on fd " << fd << ", errno: " << errno << ": " << strerror(errno) << std::endl;
+        return -1;
+    }
+    if (debug) std::cout << "has_data: fd=" << fd << ", rc=" << rc << std::endl;
+    return rc; // 1 indicates data ready, 0 indicates no data
+}
+
 #endif 
